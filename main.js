dd98c43c49f5057013f0a58ce5c57daeaae220ed
diep <diep.le@0004s.com>
2021-08-05 18:37:09 +0900

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+const deg = 6
+const hr = document.getElementById('hr')
+const mn = document.getElementById('mn')
+const sc = document.getElementById('sc')
+
+setInterval(() => {
+  let now = new Date()
+  let hh = now.getHours() * 30
+  let mm = now.getMinutes() * deg
+  let ss = now.getSeconds() * deg
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`
+  mn.style.transform = `rotateZ(${360 + mm}deg)`
+  sc.style.transform = `rotateZ(${360 + ss}deg)`
+
+});
