9c2f48d2147278a6038384843aff9588974dce24
diep <diep.le@0004s.com>
2021-08-06 14:47:23 +0900

    update process

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ setInterval(() => {
   let now = new Date()
   let hh = now.getHours() * 30
   let mm = now.getMinutes() * deg
-  let ss = now.getSeconds() * deg
+  let ss = (now.getSeconds() + 1) * deg
 
   hr.style.transform = `rotateZ(${hh + mm / 12}deg)`
   mn.style.transform = `rotateZ(${360 + mm}deg)`
   sc.style.transform = `rotateZ(${360 + ss}deg)`
   sc.style.setProperty("--r", ss / 360)
-});
+}, 1000);
